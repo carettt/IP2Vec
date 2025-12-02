323f00e0417d9650fd230ffcfa1b18ef4054369b
caret_ <dandradepac@outlook.com>
2025-12-02 16:21:10 +0000

    Linebreaker trainer configuration

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use anyhow::Result;
+use anyhow::{Context, Result};
 
 use burn::{backend::{libtorch::LibTorchDevice, Autodiff}, optim::SgdConfig};
 use ip2vec::{
@@ -9,16 +9,19 @@ fn main() -> Result<()> {
   let device = LibTorchDevice::Cuda(0);
 
   let trainer = TrainingConfig::new(
-    String::from("/home/caret/Documents/cmp400/model"),
+    String::from("/home/jovyan/work/model"),
     Ip2VecConfig::new(),
     SgdConfig::new())
-      .with_batch_size(1024)
+      .with_batch_size(4096)
       .with_epochs(20)
-      .with_threads(16);
+      .with_learning_rate(1.0e-3)
+      .with_threads(128);
 
-  let dataset = Ip2VecDataset::import_dataset("../NF-UNSW-NB15-v3/data/NF-UNSW-NB15-v3.csv")?;
+  let dataset = Ip2VecDataset::import_dataset("/home/jovyan/work/NF-UNSW-NB15-v3.csv")
+    .context("failed to import dataset")?;
 
-  trainer.init::<Tch>(&device)?;
+  trainer.init::<Tch>(&device)
+    .context("failed to initialize trainer")?;
   trainer.train::<Autodiff<Tch>>(dataset, &device);
 
   Ok(())
