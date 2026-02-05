1ab90088290a4cca800c37478c4e014d949c4a66
caret_ <dandradepac@outlook.com>
2026-02-05 17:02:07 +0000

    add training parameter flags and logic

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,39 @@
+use std::path::PathBuf;
+
 use anyhow::{Context, Result};
 
 use burn::{backend::{libtorch::LibTorchDevice, Autodiff}, optim::SgdConfig};
+use clap::Parser;
+
 use ip2vec::{
-  dataset::Ip2VecDataset, model::Ip2VecConfig, train::TrainingConfig, Tch
+  dataset::Ip2VecDataset, interface::{Arguments}, model::Ip2VecConfig, train::{TrainingConfig, ApplyOption}, Tch
 };
 
 fn main() -> Result<()> {
+  let args = Arguments::parse();
+
   let device = LibTorchDevice::Cuda(0);
 
   let trainer = TrainingConfig::new(
-    String::from("/home/jovyan/work/model"),
+    args.output.unwrap_or_else(|| PathBuf::from("./model")),
     Ip2VecConfig::new(),
-    SgdConfig::new())
-      .with_batch_size(64)
-      .with_epochs(20)
-      .with_learning_rate(1.0e-3)
-      .with_threads(128);
+    SgdConfig::new()
+    //  .with_batch_size(64)
+    //  .with_epochs(20)
+    //  .with_learning_rate(1.0e-3)
+    //  .with_threads(128);
+  );
 
-  let dataset = Ip2VecDataset::import_dataset("/home/jovyan/work/NF-UNSW-NB15-v3.csv")
+  let trainer = trainer
+    .apply_opt(TrainingConfig::with_seed, args.params.seed)
+    .apply_opt(TrainingConfig::with_epochs, args.params.epochs)
+    .apply_opt(TrainingConfig::with_split_ratio, args.params.split_ratio)
+    .apply_opt(TrainingConfig::with_batch_size, args.params.batch_size)
+    .apply_opt(TrainingConfig::with_threads, args.params.threads)
+    .apply_opt(TrainingConfig::with_learning_rate, args.params.learning_rate)
+    .apply_opt(TrainingConfig::with_context_window, args.params.context_window);
+
+  let dataset = Ip2VecDataset::import_dataset(&args.dataset)
     .context("failed to import dataset")?;
 
   trainer.init::<Tch>(&device)
